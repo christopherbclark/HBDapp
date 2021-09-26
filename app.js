06c99d73fa91e70ada841ef64c1bebc348fe783c
christopherbclark <clark.ny@gmail.com>
2021-09-26 09:09:12 -0400

    Basic application updates.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server!');
-});
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', viewRouter);
 
 module.exports = app;
